1e304b92e217cd819b726a5a5eefce491ab8dbb1
trohat <petr.sed@centrum.cz>
2017-08-22 12:49:21 +0200

    Pohyby palek

--- hra.js
+++ hra.js
@@ -1,6 +1,6 @@
-const HRAJEM = false; 
-// přepíná různé módy testování
-// vymazat ve finální verzi
+const HRAJEM = true; 
+// prepina ruzne mody testovani
+// vymazat ve finalni verzi
 
 var hra = {
 
@@ -11,14 +11,15 @@ var hra = {
 		vratNaZacatek() {
 			this.posX = this.startposX;
 			this.posY = this.startposY;
-			this.smerX = Math.random() > 0.5 ? -3 : 3; // vymaz
-			this.smerY = Math.random() > 0.5 ? -0.5 : 0.5; // vymaz
+			this.smerX = Math.random() > 0.5 ? -3 : 3; 
+			this.smerY = Math.random() > 0.5 ? -0.5 : 0.5; 
 		}
-		},
+	},
 
 	palka1: {},
 	palka2: {},
 	hriste: {},
+	klavesy: {}, //stisknute klavesy
 	skore1: {
 		hodnota: 0,
 		kde: document.getElementById("skore1")
@@ -29,10 +30,10 @@ var hra = {
 	},
 	interval: 0,
 	ostartovana: false	
-}; // globální objekt ve kterém jsou schované všechny menší objekty
+}; // globalni objekt ve kterem jsou schovane vsechny mensi objekty
 
 function nastavPoziciObjektu (nepouzita_promenna_objekt, idcko) {   
-	var objekt = hra[idcko];
+	var objekt = hra[idcko]; // objekt nakonec zvladnu precist pomoci idcka, takze nemusel byt v parametrech
 	objekt.ja = document.getElementById(idcko),
 	objekt.posX = objekt.ja.offsetLeft;
 	objekt.posY = objekt.ja.offsetTop;
@@ -42,20 +43,20 @@ function nastavPoziciObjektu (nepouzita_promenna_objekt, idcko) {
 		objekt.startposX = objekt.posX;
 		objekt.startposY = objekt.posY;
 	}
-} // pozice míčků a pálek
-// idcko je český výraz pro slovo id - čeština mi pomáhá vyhnout se klíčovým slovům
+} // pozice micku a palek
+// idcko je cesky vyraz pro slovo id - cestina mi pomaha vyhnout se klicovym slovum
 
 function nastavPoziciHriste(hriste) {
 	hriste.ja = document.getElementById("hriste");
 	hriste.sirka = hriste.ja.clientWidth;
 	hriste.vyska = hriste.ja.clientHeight;
-} // potřebuje samostatnou funkci, protože používá clientWidth místo offsetWidth
+} // potrebuje samostatnou funkci, protoze pouziva clientWidth misto offsetWidth
 
 hra.hraj = function(){
-	console.log ("hrajem");
+	console.log (hra.klavesy); // vymazat
 	hra.pohniMickem();
-	// todo - pohniPalkami
-}; // cyklus spuštěný setIntervalem ve funkci zmenStav
+	hra.pohniPalkami();
+}; // cyklus spusteny setIntervalem ve funkci zmenStav
 
 hra.zmenStav = function(){
 	hra.odstartovana = !hra.odstartovana;
@@ -65,16 +66,16 @@ hra.zmenStav = function(){
 	} else {
 		clearInterval(hra.interval);
 	}
-}; // odstartuje hru - nebo ji zastaví, pokud je už ostartovaná
+}; // odstartuje hru - nebo ji zastavi, pokud je uz ostartovana
 
 hra.pohniMickem = function() {
 	const mic = hra.micek;
 	if (mic.posX < 0) {
-		if (HRAJEM) { // vymazat a nechat jen IF část
+		if (HRAJEM) { // vymazat a nechat jen IF cast
 			hra.skore2.kde.innerHTML = ++hra.skore2.hodnota;
 			mic.vratNaZacatek();
 		} else {
-			mic.smerX = -mic.smerX;
+			mic.smerX = -mic.smerX; // nedojde k chybe a pripocitani bodu, micek se odrazi zpatky
 		}
 	}	
 	if (mic.posX + mic.sirka > hra.hriste.sirka) {
@@ -82,7 +83,7 @@ hra.pohniMickem = function() {
 			hra.skore1.kde.innerHTML = ++hra.skore1.hodnota;
 			mic.vratNaZacatek();
 		} else {
-			mic.smerX = -mic.smerX;
+			mic.smerX = -mic.smerX; // nedojde k chybe, micek se odrazi zpatky (varianta pro testovani)
 		}
 	}
 	if (mic.posY < 0) {
@@ -95,7 +96,41 @@ hra.pohniMickem = function() {
 	mic.posY += mic.smerY;
 	mic.ja.style.left = mic.posX+"px";
 	mic.ja.style.top = mic.posY+"px";
-}
+};
+
+hra.pohniPalkami = function() {
+	const klavesy = hra.klavesy,
+		  palka1 = hra.palka1,
+		  palka2 = hra.palka2;
+	if (klavesy["w"] || klavesy["W"]) {
+		palka1.posY -= 2;
+		if (palka1.posY < 0) {
+			palka1.posY = 0; 
+		}
+		palka1.ja.style.top = palka1.posY+"px";
+	}
+	if (klavesy["s"] || klavesy["S"]) {
+		palka1.posY += 2;
+		if (palka1.posY > hra.hriste.vyska - palka1.vyska) {
+			palka1.posY = hra.hriste.vyska - palka1.vyska; 
+		}
+		palka1.ja.style.top = palka1.posY+"px";
+	}
+	if (klavesy["ArrowUp"]) {
+		palka2.posY -= 2;
+		if (palka2.posY < 0) {
+			palka2.posY = 0; 
+		}
+		palka2.ja.style.top = palka2.posY+"px";
+	}
+	if (klavesy["ArrowDown"]) {
+		palka2.posY += 2;
+		if (palka2.posY > hra.hriste.vyska - palka2.vyska) {
+			palka2.posY = hra.hriste.vyska - palka2.vyska; 
+		}
+		palka2.ja.style.top = palka2.posY+"px";
+	}
+};
 
 function zacniHru() {
 	nastavPoziciHriste (hra.hriste);
@@ -109,3 +144,16 @@ zacniHru();
 const tlacitko = document.getElementById("start");
 
 tlacitko.addEventListener("click", hra.zmenStav);
+
+document.addEventListener("keydown", function(event) {
+	hra.klavesy[event.key] = true;
+});
+
+
+document.addEventListener("keyup", function(event) {
+	hra.klavesy[event.key] = false;
+});
+
+//asi by to šlo jeste lepe pospojovat objektove nebo pomocí ES6 trid (napr. bych vyuzil dedicnost -
+//nastavPozici by mohla byt metoda predka a ostatni by z ní dedili, a treba hriste by si ji predefinovalo
+//ale vzhledem k rozsahu programu mi prijde tohle fajn, protože je to prehledne
